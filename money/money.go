cb7f7b984064509b8495612280812922d926c88d
Yusuke Aono <yusukeaono.1031@gmail.com>
2025-05-06 22:34:54 +0900

    fix: Fix money structure (chapter 6)

--- money/money.go
+++ money/money.go
@@ -21,41 +21,11 @@ func (m Money) Equals(object any) bool {
 }
 
 // Dollar
-type Dollar struct {
-	Money
-}
-
-func NewDollar(amount int) Dollar {
-	return Dollar{Money: NewMoney(amount)}
-}
-
-func (d Dollar) Times(multiplier int) Dollar {
-	return NewDollar(d.Money.amount * multiplier)
-}
-
-func (d Dollar) Equals(object any) bool {
-	if otherDollar, ok := object.(Dollar); ok {
-		return d.Money.Equals(otherDollar.Money)
-	}
-	return false
+func NewDollar(amount int) Money {
+	return NewMoney(amount)
 }
 
 // Franc
-type Franc struct {
-	Money
-}
-
-func NewFranc(amount int) Franc {
-	return Franc{Money: NewMoney(amount)}
-}
-
-func (f Franc) Times(multiplier int) Franc {
-	return NewFranc(f.Money.amount * multiplier)
-}
-
-func (f Franc) Equals(object any) bool {
-	if otherFranc, ok := object.(Franc); ok {
-		return f.Money.Equals(otherFranc.Money)
-	}
-	return false
+func NewFranc(amount int) Money {
+	return NewMoney(amount)
 }
