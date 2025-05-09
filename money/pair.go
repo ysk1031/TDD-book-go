c537f6095ff94b6c38e8b068b3c69762a346b95d
Yusuke Aono <yusukeaono.1031@gmail.com>
2025-05-09 18:59:26 +0900

    refactor: Remove unused code

--- money/pair.go
+++ money/pair.go
@@ -8,14 +8,3 @@ type Pair struct {
 func NewPair(from, to string) Pair {
 	return Pair{From: from, To: to}
 }
-
-func (p Pair) Equals(other any) bool {
-	if otherPair, ok := other.(Pair); ok {
-		return p.From == otherPair.From && p.To == otherPair.To
-	}
-	return false
-}
-
-func (p Pair) HashCode() int {
-	return 0
-}
